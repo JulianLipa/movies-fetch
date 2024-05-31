1d18d29344b746020cfbbdac6e5b4afd83a00a2f
JulianLipa <lipajulianp@gmail.com>
2024-05-30 21:07:22 -0300

    grid movies

--- src/app/components/FetchMovies/FetchMovies.js
+++ src/app/components/FetchMovies/FetchMovies.js
@@ -1,48 +1,51 @@
-'use client'
+"use client";
 
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
+import Link from "next/link";
 
 const FetchMovies = () => {
-    const [movies, setMovies] = useState([]);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      setLoading(true);
-      const handleFetchMovies = async () => {
-        try {
-          const response = await axios.get(
-            "https://api.themoviedb.org/3/discover/movie?api_key=eb7e3fd7272143562cec959061b5eb32&with_genres=27"
-          );
-          const data = response.data.results;
-          setMovies(data);
-          console.log(data);
-          setLoading(false);
-        } catch (error) {
-          console.log("error", error);
-        }
-      };
-  
-      handleFetchMovies();
-    }, []);
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    return(
-      <div>
-        {loading && <p>Fetching data</p>}
-        {!loading && 
-        movies.map((movie,index)=>{
-          return <Image
-          key={index}
-          src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-          alt="cover image"
-          width={200}
-          height={270}
-        />
-        })
-        }
-      </div>
-    );
-}
+  useEffect(() => {
+    setLoading(true);
+    const handleFetchMovies = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.themoviedb.org/3/discover/movie?api_key=eb7e3fd7272143562cec959061b5eb32&with_genres=27"
+        );
+        const data = response.data.results;
+        setMovies(data);
+        console.log(data);
+        setLoading(false);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    handleFetchMovies();
+  }, []);
+
+  return (
+    <div>
+      {loading && <p>Fetching data</p>}
+      {!loading &&
+        movies.map((movie, index) => {
+          return (
+            <Link key={index} href={`/movies/${movie.id}`}>
+              <Image
+                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                alt="cover image"
+                width={200}
+                height={270}
+              />
+            </Link>
+          );
+        })}
+    </div>
+  );
+};
 
 export default FetchMovies;

b29a3ccc3998766ae6c6b6b6e942661a82ad33a7
orianacappelli <orianacappelli1@gmail.com>
2024-06-13 18:03:25 -0300

    diseño de pelicula

--- src/app/movies/[id]/page.js
+++ src/app/movies/[id]/page.js
@@ -4,9 +4,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
 import styles from "./movies.module.css";
-import FetchCast from "@/app/components/FetchCast/FetchCast";
-import FetchStreams from "@/app/components/FetchStreams/FetchStreams";
-import FetchGenres from "@/app/components/FetchGenres/FetchGenres";
 
 const SingleMovie = ({ params }) => {
   const { id } = params;
@@ -14,7 +11,6 @@ const SingleMovie = ({ params }) => {
   const [images, setImages] = useState({ filePath: "", width: 0, height: 0 });
   const [loading, setLoading] = useState(true);
   const [cast, setCast] = useState({ director: "", cast: "" });
-  const [stream, setStreams] = useState({});
 
   useEffect(() => {
     const handleFetchMovies = async () => {
@@ -24,14 +20,13 @@ const SingleMovie = ({ params }) => {
           `https://api.themoviedb.org/3/movie/${id}?api_key=eb7e3fd7272143562cec959061b5eb32`
         );
         const year = response.data.release_date.split("-")[0];
+        setLoading(false);
         setMovie({
           movie: response.data,
           year: year,
         });
       } catch (error) {
         console.log("error", error);
-      } finally {
-        setLoading(false);
       }
     };
 
@@ -42,6 +37,7 @@ const SingleMovie = ({ params }) => {
           `https://api.themoviedb.org/3/movie/${id}/images?api_key=eb7e3fd7272143562cec959061b5eb32`
         );
         const data = response.data.backdrops[0];
+        setLoading(false);
         setImages({
           filePath: data.file_path,
           width: data.width,
@@ -49,8 +45,6 @@ const SingleMovie = ({ params }) => {
         });
       } catch (error) {
         console.log("error", error);
-      } finally {
-        setLoading(false);
       }
     };
 
@@ -60,50 +54,31 @@ const SingleMovie = ({ params }) => {
         const response = await axios.get(
           `https://api.themoviedb.org/3/movie/${id}/credits?api_key=eb7e3fd7272143562cec959061b5eb32`
         );
-        const cast = response.data.cast.slice(1, 5);
+        const cast = response.data.cast.slice(0, 3);
         let crew = response.data.crew;
         let director;
 
         for (let i = 0; i < crew.length; i++) {
           if (crew[i].job === "Director") {
             director = crew[i];
-            break;
+            break; // Exit loop once the director is found
           }
         }
+
+        setLoading(false);
         setCast({
           cast: cast,
           director: director,
         });
       } catch (error) {
         console.log("error", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    const handleWatchStreams = async () => {
-      setLoading(true);
-      try {
-        const response = await axios.get(
-          `https://api.themoviedb.org/3/movie/${id}/watch/providers?api_key=eb7e3fd7272143562cec959061b5eb32`
-        );
-        const data = response.data.results.US;
-
-        setStreams(data);
-      } catch (error) {
-        console.log("error", error);
-      } finally {
-        setLoading(false);
       }
     };
 
     handleFetchMovies();
     handleMoviesFrames();
     handleCast();
-    handleWatchStreams();
   }, [id]);
-
-  console.log(movie);
 
   return (
     <div>
@@ -115,51 +90,51 @@ const SingleMovie = ({ params }) => {
           height={images.height}
         />
       )}
-
-      {!loading && movie && images.filePath && stream != "" && cast != "" && (
+      {!loading && movie && images.filePath && (
         <div className={`${styles["container_single_movies"]}`}>
-          <Image
-            className={`${styles["img_hero"]}`}
-            src={`https://image.tmdb.org/t/p/original${images.filePath}`}
-            alt="cover image"
-            width={images.width}
-            height={images.height}
-          />
-          <div>
-            <p>{movie.year}</p>
-            <h2>{movie.movie.title}</h2>
-            <p>{movie.movie.overview}</p>
+         <div className={"container_hero"}>
+            <div className={`${styles["overlay"]}`}></div>
             <Image
+              className={`${styles["img_hero"]}`}
+              src={`https://image.tmdb.org/t/p/original${images.filePath}`}
+              alt="cover image"
+              width={images.width}
+              height={images.height}
+            />
+         </div>
+          
+          <div className={`${styles["content"]}`}>
+            <h1>{movie.movie.title}</h1>
+
+            <div className={`${styles["año_genero"]}`}>
+              <h3>{movie.year}</h3>
+              <div>
+                <h2>Genres</h2>
+                {/* Componente de genres */}
+              </div>
+            </div>
+
+            <p className={`${styles["info"]}`}>{movie.movie.overview}</p>
+            {/*<Image
               src={`https://image.tmdb.org/t/p/original${movie.movie.poster_path}`}
               alt="cover image"
               width={200}
               height={270}
-            />
-
-            <div>
-              <h2>Director</h2>
-              <p>{cast.director.name}</p>
-              <Image
+            />*/}
+            <div className={`${styles["direccion"]}`}>
+              <h2>Director:</h2>
+              <h3>{cast.director.name}</h3>
+             {/* <Image
                 src={`https://image.tmdb.org/t/p/original${cast.director.profile_path}`}
                 alt="cover image"
                 width={100}
                 height={150}
-              />
+              />*/}
             </div>
 
             <div>
-              <FetchStreams streams={stream} />
-            </div>
-
-            <div>
-              <h2>Cast</h2>
-              <FetchCast cast={cast.cast} />
+              <h2>Cast:</h2>
               {/* Componente de cast */}
-            </div>
-
-            <div>
-              <h2>Genres</h2>
-              <FetchGenres genres={movie.movie.genres}/>
             </div>
           </div>
         </div>
